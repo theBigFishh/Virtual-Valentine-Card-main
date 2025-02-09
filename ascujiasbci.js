48928c997c4799e5b318478caa90d3656b14ee8c
theBigFishh <krishyvaswani@gmail.com>
2025-02-09 21:07:06 +0200

    Add files via upload

---
+++ ascujiasbci.js
@@ -0,0 +1,36 @@
+function start () {
+  // (PART A) GET SPLASH SCREEN
+  let splash = document.getElementById("splash");
+ 
+  // (PART B) PLAY BGM & REMOVE SPLASH SCREEN AFTER FADE IN
+  splash.addEventListener("transitionend", () => {
+    document.getElementById("bgm").play();
+    splash.remove();
+  });
+ 
+  // (PART C) GO!
+  splash.classList.add("hide");
+}
+
+
+let images = document.querySelectorAll("img");
+let wrapper = document.getElementById("wrapper");
+let imgWrapper = document.getElementById("fullImg");
+let close = document.querySelector("span");
+
+
+
+images.forEach((img, index) => {
+  img.addEventListener("click", () => {
+    openModal(`Yami${index}.png`);
+
+  });
+});
+
+close.addEventListener("click", () => (wrapper.style.display = "none"));
+
+function openModal(pic) {
+  wrapper.style.display = "flex";
+  imgWrapper.src = pic;
+}
+
